5f15a7c363fd05ffb9f0107a36f2190115aa4f36
Do Truong <dotruong0704@gmail.com>
2024-03-21 03:18:50 +0700

    đổi ngược chat

--- src/features/chat/index.js
+++ src/features/chat/index.js
@@ -5,8 +5,33 @@ function Chat() {
     return (
         <>
             <TitleCard title="Linh Be" topMargin="mt-2">
-                <div className="h-[35rem]">
-
+                <div className="h-[35rem] flex flex-col-reverse">
+                    <form className="sticky top-0 mt-4">
+                        <label
+                            for="search"
+                            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+                        >
+                            Search
+                        </label>
+                        <div className="relative">
+                            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                <ChatBubbleBottomCenterTextIcon className="w-5 text-primary" />
+                            </div>
+                            <input
+                                type="search"
+                                id="search"
+                                className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Nhập tin nhắn..."
+                                required
+                            />
+                            <button
+                                type="submit"
+                                className="flex items-center text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-4 py-2 "
+                            >
+                                Gửi <PaperAirplaneIcon className="w-5" />
+                            </button>
+                        </div>
+                    </form>
                     <div className="chat chat-start">
                         <div className="chat-image avatar">
                             <div className="w-10 rounded-full">
@@ -40,32 +65,7 @@ function Chat() {
                         <div className="chat-footer opacity-50">Seen at 12:46</div>
                     </div>
 
-                    <form className="sticky top-0">
-                        <label
-                            for="search"
-                            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-                        >
-                            Search
-                        </label>
-                        <div className="relative">
-                            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                <ChatBubbleBottomCenterTextIcon className="w-5 text-primary" />
-                            </div>
-                            <input
-                                type="search"
-                                id="search"
-                                className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="Nhập tin nhắn..."
-                                required
-                            />
-                            <button
-                                type="submit"
-                                className="flex items-center text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-4 py-2 "
-                            >
-                                Gửi <PaperAirplaneIcon className="w-5" />
-                            </button>
-                        </div>
-                    </form>
+
                 </div>
             </TitleCard>
         </>
