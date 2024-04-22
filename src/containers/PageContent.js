dcc82e05f96d49359aa02a2fbe5fbb565f70f9e7
truongsut21 <dotruong0704@gmail.com>
2024-04-22 16:04:56 +0700

    fix giao dien

--- src/containers/PageContent.js
+++ src/containers/PageContent.js
@@ -45,7 +45,7 @@ function PageContent(){
                             <Route path="*" element={<Page404 />} />
                         </Routes>
                 </Suspense>
-                <div className="h-16"></div>
+                {/* <div className="h-16"></div> */}
             </main>
         </div> 
     )
